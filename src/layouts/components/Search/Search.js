7a929b5fc5556da1e016f23d8be2747b786bb2d1
Quang Linh <nguyenquanglinh0509@gmail.com>
2026-01-02 15:01:36 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,100 @@
+import { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames/bind';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import HeadlessTippy from '@tippyjs/react/headless';
+
+import * as searchServices from '~/services/searchService';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import SearchAccountsItem from '~/components/SearchAccountsItem';
+import styles from './Search.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { SearchIcon } from '~/components/Icons';
+import { useDebounce } from '~/hooks';
+const cx = classNames.bind(styles);
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const debounced = useDebounce(searchValue, 500);
+    const inputRef = useRef();
+
+    useEffect(() => {
+        if (!debounced.trim()) {
+            setSearchResult([]);
+            return;
+        }
+        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
+            const result = await searchServices.search(debounced);
+            const top5 = result.slice(0, 5);
+            setSearchResult(top5);
+            setLoading(false);
+        };
+        fetchApi();
+    }, [debounced]);
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+    const handleChange = (e) => {
+        const searchValueChange = e.target.value;
+        if (searchValueChange.startsWith(' ')) return;
+        setSearchValue(searchValueChange);
+    };
+
+    return (
+        //Using a wrapper <div> or <span> tag around the reference element solves this by creating a new parentNode context.
+        <div>
+            <HeadlessTippy
+                interactive
+                visible={showResult && searchResult.length > 0}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            {searchResult.map((result) => (
+                                <SearchAccountsItem key={result.id} data={result} />
+                            ))}
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHideResult}
+            >
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear')} onClick={handleClear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+
+                    {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+
+                    <button
+                        className={cx('search-btn')}
+                        onMouseDown={(e) => {
+                            e.preventDefault();
+                        }}
+                    >
+                        <SearchIcon />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        </div>
+    );
+}
+
+export default Search;
