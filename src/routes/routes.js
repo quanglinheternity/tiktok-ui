6f3b4d8ee80a43d4833cbc3548a1afd0e1db9bc2
Quang Linh <nguyenquanglinh0509@gmail.com>
2026-01-02 16:28:32 +0700

    làm ui phần sider 1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,10 +5,12 @@ import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
 ];
